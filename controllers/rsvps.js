2d63df032f388f2fcdc321a2accfd643b2fa81ec
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-04-01 21:52:29 -0700

    added delete rsvp

--- controllers/rsvps.js
+++ controllers/rsvps.js
@@ -1,6 +1,6 @@
 module.exports = (app, models) => {
-       // Create
-       app.post('/events/:eventId/rsvps', (req, res) => {
+    // Create
+    app.post('/events/:eventId/rsvps', (req, res) => {
         console.log('Hello')
         req.body.EventId = req.params.eventId;
         console.log(req.body);
@@ -10,20 +10,30 @@ module.exports = (app, models) => {
             console.error(err)
         });
     });
-    
+
     // New
     app.get('/events/:eventId/rsvps/new', (req, res) => {
         models.Event.findByPk(req.params.eventId).then(event => {
-            res.render('rsvps-new', {event: event});
+            res.render('rsvps-new', { event: event });
         });
     });
 
     // Show
     app.get('/events/:id', (req, res) => {
-        models.Event.findByPk(req.params.id, {include: [{model: models.Rsvp}]}).then(event => {
-            res.render('events-show', {event: event});
+        models.Event.findByPk(req.params.id, { include: [{ model: models.Rsvp }] }).then(event => {
+            res.render('events-show', { event: event });
         }).catch((err) => {
             console.error(err);
         })
+    });
+
+    // DELETE
+    app.delete('/events/:eventId/rsvps/:id', (req, res) => {
+        models.Rsvp.findByPk(req.params.id).then(rsvp => {
+            rsvp.destroy();
+            res.redirect(`/events/${req.params.eventId}`);
+        }).catch((err) => {
+            console.log(err);
+        });
     });
 } 
